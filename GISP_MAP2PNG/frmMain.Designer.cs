8e878ca611fee0c2f794fb0e0ade626f5075c13a
gmolina75 <gmolina75@gmail.com>
2024-12-19 14:23:52 -0500

    Add project files.

---
+++ GISP_MAP2PNG/frmMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GISP_MAP2PNG
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainMap
+            // 
+            this.MainMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainMap.Bearing = 0F;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MainMap.LevelsKeepInMemory = 5;
+            this.MainMap.Location = new System.Drawing.Point(12, 43);
+            this.MainMap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 2;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomEnabled = true;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.NegativeMode = false;
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(909, 506);
+            this.MainMap.TabIndex = 0;
+            this.MainMap.Zoom = 0D;
+            this.MainMap.OnTileLoadComplete += new GMap.NET.TileLoadComplete(this.MainMap_OnTileLoadComplete);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(737, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Imprimir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 549);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainMap);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmMain";
+            this.Text = "Imagen To Png";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GISP_MAP2PNG/frmMain.cs
@@ -0,0 +1,187 @@
+﻿using DATAMAP;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GISP_MAP2PNG
+{
+    public partial class frmMain : Form
+    {
+        string[] args = null;
+        bool autoclose = false;
+        bool saved = false;
+        string filename = "";
+        public frmMain()
+        {
+            InitializeComponent();
+
+
+         
+          
+            ///
+        }
+        private async Task SaveMapAsync()
+        {
+            try
+            {
+                if (MainMap?.IsDisposed != false)
+                    return;
+
+                await Task.Delay(5000);
+                if (MainMap?.IsDisposed != false)
+                    return;
+                if (saved)
+                    return;
+                using (var tmpImage = MainMap.ToImage())
+                {
+                    if (tmpImage != null)
+                    {
+                        if (File.Exists(filename))
+                            File.Delete(filename);
+                        tmpImage.Save(filename);
+                        saved = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al guardar el mapa: {ex.Message}");
+                CloseForm();
+            }
+        }
+        //public bool saveMap()
+        //{
+        //    try
+        //    {
+        //        if(MainMap==null || MainMap.IsDisposed )
+        //        {
+        //            return false;
+        //        }
+        //        Thread.Sleep(5000);
+        //        if (MainMap == null || MainMap.IsDisposed)
+        //        {
+        //            return false;
+        //        }
+        //        Image tmpImage = MainMap.ToImage();
+        //        if (tmpImage != null)
+        //        {
+        //            using (tmpImage)
+        //            {
+        //                filename = args[2];
+        //                if(File.Exists(filename))
+        //                    File.Delete(filename);
+        //                tmpImage.Save(filename);
+        //                return true;
+        //                //string base64 = ClsImgConvert.imagePathTobase64(filename);
+        //                //Console.WriteLine(base64);
+        //                //System.Diagnostics.Debug.WriteLine(base64);
+
+        //            }
+        //        }
+        //        //Thread.Sleep(5000);
+        //    }
+        //    catch (Exception)
+        //    {
+
+
+        //    }
+        //    return false;
+        //}
+
+        private void ConfigureMap()
+        {
+            MainMap.MapProvider = GMapProviders.GoogleMap;
+            MainMap.Position = new PointLatLng(ClsValida.DVal(args[0]), ClsValida.DVal(args[1]));
+            MainMap.MinZoom = 1;
+            MainMap.MaxZoom = 24;
+            MainMap.Zoom = 15;
+
+            var markersOverlay = new GMapOverlay("markers");
+            var marker = new GMarkerGoogle(MainMap.Position, GMarkerGoogleType.blue_small);
+
+            if (args.Length >= 5)
+            {
+                marker.ToolTipMode = MarkerTooltipMode.Always;
+                marker.ToolTipText = args[4];
+            }
+
+            markersOverlay.Markers.Add(marker);
+            MainMap.Overlays.Add(markersOverlay);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                args = Environment.CommandLine.Split('/').Skip(1).ToArray();
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("No hay suficientes argumentos.");
+                    CloseForm();
+                    return;
+                }
+
+                ConfigureMap();
+
+                filename = args[2];
+                autoclose = args.Length > 3 && args[3] == "autoclose";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                CloseForm();
+            }
+        }
+        private void CloseForm()
+        {
+            if (!IsDisposed)
+            {
+                this.Close();
+            }
+        }
+
+        // loader end loading tiles
+        void MainMap_OnTileLoadComplete(long ElapsedMilliseconds)
+        {
+
+            //MainMap.ElapsedMilliseconds = ElapsedMilliseconds;
+
+            MethodInvoker m = async delegate ()
+            {
+                if (!IsDisposed)
+                {
+                    if(!saved)
+                        await SaveMapAsync();
+
+                    CloseForm();
+                      
+                    
+                  
+                }
+                //panelMenu.Text = "Menu, Carga en " + MainMap.ElapsedMilliseconds + "ms";
+
+                //textBoxMemory.Text = string.Format(CultureInfo.InvariantCulture, "{0:0.00} MB de {1:0.00} MB", MainMap.Manager.MemoryCache.Size, MainMap.Manager.MemoryCache.Capacity);
+            };
+            try
+            {
+                BeginInvoke(m);
+            }
+            catch
+            {
+                CloseForm();
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SaveMapAsync();
+        }
+    }
+}
